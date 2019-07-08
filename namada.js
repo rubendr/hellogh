fc7504d11f044a6df5fee1926c46a7c7643f3d25
Ruben <rdrm41@gmail.com>
2019-07-08 09:57:47 +0700

    ok

--- namada.js
+++ namada.js
@@ -1,82 +1,80 @@
 var width = 340;// 320; // We will scale the photo width to this
-    var height = 280;// 0; // This will be computed based on the input stream
-    var streaming = false;
-    var video = null;
-    var canvas = null;
-    var photo = null;
-    var startbutton = null;
-    var video2 = null;
-    function startup() {
-      video = document.getElementById('video');
-      canvas = document.getElementById('canvas');
-      photo = document.getElementById('photo');
-      photodata = document.getElementById('photodata');
+var height = 280;// 0; // This will be computed based on the input stream
+var streaming = false;
+var video = null;
+var canvas = null;
+var photo = null;
+var startbutton = null;
+var video2 = null;
+function startup() {
+    video = document.getElementById('video');
+    canvas = document.getElementById('canvas');
+    photo = document.getElementById('photo');
+    photodata = document.getElementById('photodata');
 
-      video2 = document.getElementById('video2');
-      navigator.mediaDevices.getUserMedia({ audio: false, video: true })
+    video2 = document.getElementById('video2');
+    navigator.mediaDevices.getUserMedia({ audio: false, video: true })
         .then(function (stream) {
-          video.srcObject = stream;
-          video.play();
-          video2.srcObject = stream;
-          video2.play();
+            video.srcObject = stream;
+            video.play();
+            video2.srcObject = stream;
+            video2.play();
         })
         .catch(function (err) {
-          console.log("An error occured! " + err);
-          //alert("An error occured! " + err);
-          $("#lblcamera").html("CAM:" + err);
+            console.log("An error occured! " + err);
         });
-      video.addEventListener('canplay', function (ev) {
+    video.addEventListener('canplay', function (ev) {
         if (!streaming) {
-          height = video.videoHeight / (video.videoWidth / width);
-          video.setAttribute('width', width);
-          video.setAttribute('height', height);
-          video.setAttribute('facingMode', 'user');
-          canvas.setAttribute('width', width);
-          canvas.setAttribute('height', height);
-          streaming = true;
+            height = video.videoHeight / (video.videoWidth / width);
+            video.setAttribute('width', width);
+            video.setAttribute('height', height);
+            video.setAttribute('facingMode', 'user');
+            canvas.setAttribute('width', width);
+            canvas.setAttribute('height', height);
+            streaming = true;
         }
-      }, false);
+    }, false);
 
-      video2.addEventListener('canplay', function (ev) {
+    video2.addEventListener('canplay', function (ev) {
         if (!streaming) {
-          height = video2.videoHeight / (video2.videoWidth / width);
-          video2.setAttribute('width', width);
-          video2.setAttribute('height', height);
-          canvas.setAttribute('width', width);
-          canvas.setAttribute('height', height);
-          streaming = true;
+            height = video2.videoHeight / (video2.videoWidth / width);
+            video2.setAttribute('width', width);
+            video2.setAttribute('height', height);
+            canvas.setAttribute('width', width);
+            canvas.setAttribute('height', height);
+            streaming = true;
         }
-      }, false);
-      video.addEventListener('unload', function (e) {
+    }, false);
+    video.addEventListener('unload', function (e) {
         alert('unloaded video');
-      });
-      video.addEventListener('onload', function () {
+    });
+    video.addEventListener('onload', function () {
         alert('loaded');
-      });
-      clearphoto();
-    }
+    });
+    clearphoto();
+}
 
-    function clearphoto() {
-      var context = canvas.getContext('2d');
-      context.fillStyle = "#AAA";
-      context.fillRect(0, 0, canvas.width, canvas.height);
-      var data = canvas.toDataURL('image/png');
-      photo.setAttribute('src', data);
+function clearphoto() {
+    var context = canvas.getContext('2d');
+    context.fillStyle = "#AAA";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    var data = canvas.toDataURL('image/png');
+    photo.setAttribute('src', data);
 
-    }
+}
 
-    function takepicture() {
-      var context = canvas.getContext('2d');
-      if (width && height) {
+function takepicture() {
+    var context = canvas.getContext('2d');
+    if (width && height) {
         canvas.width = width;
         canvas.height = height;
         context.drawImage(video, 0, 0, width, height);
         var data = canvas.toDataURL('image/png');
         photo.setAttribute('src', data);
         photodata.value = data;
-      } else {
+    } else {
         clearphoto();
-      }
     }
+}
 
-    startup();
+startup();
